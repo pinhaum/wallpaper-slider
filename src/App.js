ee5e5df24030d3854bd2d98abbcb71e9ea2546b2
Gabriel Ramos <gcrepho@gmail.com>
2021-04-07 13:35:00 -0300

    initial commit for wallpaper slider

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+
+const images = [
+  'https://i.picsum.photos/id/1/200/300.jpg?hmac=jH5bDkLr6Tgy3oAg5khKCHeunZMHq0ehBZr6vGifPLY',
+  'https://i.picsum.photos/id/2/200/300.jpg?hmac=HiDjvfge5yCzj935PIMj1qOf4KtvrfqWX3j4z1huDaU',
+  'https://i.picsum.photos/id/3/200/300.jpg?hmac=o1-38H2y96Nm7qbRf8Aua54lF97OFQSHR41ATNErqFc',
+];
 
 function App() {
+  const [index, setIndex] = useState(0);
+
+  const next = () => {
+    setIndex((i) => (i + 1) % images.length);
+  };
+
+  const prev = () => {
+    setIndex((i) => (i - 1) % images.length);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={prev}>&lt;</button>
+      <img src={images[index]} alt="" />
+      <button onClick={next}>&gt;</button>
     </div>
   );
 }
